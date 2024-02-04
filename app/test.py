befa7ef11dc275971b999f0b2f05fecdc111fafa
wiznikvibe <nikhilshetty439@gmail.com>
2024-02-04 12:14:14 +0530

    SQL Integration

--- app/test.py
+++ app/test.py
@@ -3,8 +3,34 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import mysql.connector as conn
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from configparser import ConfigParser
+import time
 
 app = FastAPI()
+
+config = ConfigParser()
+config.read('config.ini')
+
+host = config['USER']['host']
+user = config['USER']['user']
+password = config['USER']['password']
+database = config['USER']['database']
+
+while True:
+    try:
+        mydb = conn.connect(host=host, user=user, password=password, database=database)
+        cursor = mydb.cursor(buffered=True, dictionary=True)
+        # tables = cursor.execute("""SHOW TABLES;""")
+        print("Connection Succefully Established")
+        # print(tables)
+        break
+    except Exception as e:
+        print("Connection Failed")
+        print("Error:", e)
+        time.sleep(2)
 
 # Class for Base Model 
 class Post(BaseModel):
@@ -13,8 +39,8 @@ class Post(BaseModel):
     published: bool = False
     
 
-### Sample Posts
-my_posts = [{"title":"title of Post 1","content":"content of Post 1","id": 1},{"title":"title of Post 2","content":"content of Post 2","id": 2}]
+# ### Sample Posts
+# my_posts = [{"title":"title of Post 1","content":"content of Post 1","id": 1},{"title":"title of Post 2","content":"content of Post 2","id": 2}]
 
 def find_post(id):
     for p in my_posts:
@@ -35,7 +61,10 @@ def root():
 # Returns all the posts 
 @app.get("/posts")
 def get_post():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
 
 # Returns the latest post 
 @app.get("/posts/latest")
@@ -55,10 +84,13 @@ def get_post(id: int):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0,10000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    # post_dict = post.dict()
+    # post_dict['id'] = randrange(0,10000)
+    # my_posts.append(post_dict)
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s)""", (post.title, post.content, post.published))
+    cursor.execute("""SELECT LAST_INSERT_ID() as inserted_id""")
+    new_post = cursor.fetchone()
+    return {"data": new_post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
@@ -81,4 +113,3 @@ def update_post(id: int, post: Post):
 
     return {'message':f'{post_dict} Updated'}
 
-# 2:34:39
