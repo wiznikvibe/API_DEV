89c14edc7c84fe3ba385337d56bec215008345d4
wiznikvibe <nikhilshetty439@gmail.com>
2024-02-10 20:37:53 +0530

    New Column Added

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from app.database import Base
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = "posts"
@@ -8,4 +10,5 @@ class Post(Base):
     title = Column(String(255), nullable=False)
     content = Column(String(255), nullable=False)
     published = Column(Boolean, default=False, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, default=text('now'))
     
